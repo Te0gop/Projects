ac0ba3b4c9851b7e28d2baff1abcfcbd0cedacdc
Te0gop <58914514+Te0gop@users.noreply.github.com>
2022-02-02 14:40:20 +0200

    Delete shop-app directory

--- shop-app/src/main/java/com/spring/shopapp/exceptions/FoodNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.spring.shopapp.exceptions;
-
-import com.spring.shopapp.entity.Foods;
-
-public class FoodNotFoundException extends Throwable{
-    public FoodNotFoundException(String message) {
-        super(message);
-    }
-}
--- shop-app/src/main/java/com/spring/shopapp/repository/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.shopapp.repository;
-
-import com.spring.shopapp.entity.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-}
