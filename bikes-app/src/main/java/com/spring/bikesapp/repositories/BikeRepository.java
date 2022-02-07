ae869288429681c4d55924ecf273fc773c7db5a1
Te0gop <58914514+Te0gop@users.noreply.github.com>
2022-02-07 13:49:39 +0200

    Delete bikes-app directory

--- bikes-app/src/main/java/com/spring/bikesapp/repositories/BikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.bikesapp.repositories;
-
-import com.spring.bikesapp.models.Bike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BikeRepository extends JpaRepository<Bike, Long> {
-}
