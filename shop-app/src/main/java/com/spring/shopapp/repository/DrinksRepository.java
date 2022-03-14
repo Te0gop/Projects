4ab1ee42bcc6b6ce2a0cb41a0be46b0b3ed04c81
Teodor <te0gop.u@gmail.com>
2022-03-14 22:53:20 +0200

    small changes in repos

--- shop-app/src/main/java/com/spring/shopapp/repository/DrinksRepository.java
+++ shop-app/src/main/java/com/spring/shopapp/repository/DrinksRepository.java
@@ -2,11 +2,14 @@ package com.spring.shopapp.repository;
 
 import com.spring.shopapp.entity.Drinks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface DrinksRepository extends JpaRepository<Drinks, Long> {
     List<Drinks> findDrinkByName(String name);
 
--- shop-app/src/main/java/com/spring/shopapp/repository/FoodsRepository.java
+++ shop-app/src/main/java/com/spring/shopapp/repository/FoodsRepository.java
@@ -3,11 +3,13 @@ package com.spring.shopapp.repository;
 import com.spring.shopapp.entity.Drinks;
 import com.spring.shopapp.entity.Foods;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface FoodsRepository extends JpaRepository<Foods, Long> {
     List<Foods> findFoodByName(String name);
 
