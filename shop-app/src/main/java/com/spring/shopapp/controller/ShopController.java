414bfe4c7c8c353e7ad87e5c98c3312e2d5a69b1
Teodor <te0gop.u@gmail.com>
2022-03-15 21:19:35 +0200

    refactoring shop's creation

--- shop-app/src/main/java/com/spring/shopapp/controller/ShopController.java
+++ shop-app/src/main/java/com/spring/shopapp/controller/ShopController.java
@@ -1,13 +1,8 @@
 package com.spring.shopapp.controller;
 
-import com.spring.shopapp.entity.Drinks;
-import com.spring.shopapp.entity.Foods;
 import com.spring.shopapp.entity.Shop;
-import com.spring.shopapp.repository.DrinksRepository;
-import com.spring.shopapp.repository.FoodsRepository;
-import com.spring.shopapp.repository.ShopRepository;
+import com.spring.shopapp.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,28 +13,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ShopController {
 
     @Autowired
-    private ShopRepository shopRepository;
-
-    @Autowired
-    private DrinksRepository drinksRepository;
-
-    @Autowired
-    private FoodsRepository foodsRepository;
+    private ShopService shopService;
 
     @RequestMapping(name = "shop", method = RequestMethod.POST)
     public ResponseEntity<Shop> createShop(@RequestBody Shop shop) {
-        shopRepository.save(shop);
-
-         for(Foods food : shop.getFoods()) {
-             food.setShop(shop);
-             foodsRepository.save(food);
-         }
-
-         for(Drinks drink : shop.getDrinks()) {
-             drink.setShop(shop);
-             drinksRepository.save(drink);
-         }
-         return new ResponseEntity<>(HttpStatus.OK);
+        return shopService.createShop(shop);
     }
 
 }
