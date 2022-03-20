ecf5ae6acaec361b1102f81ec6ad6f919ecb462f
Teodor <te0gop.u@gmail.com>
2022-03-20 13:26:00 +0200

    small changes in entities

--- shop-app/src/main/java/com/spring/shopapp/entity/Foods.java
+++ shop-app/src/main/java/com/spring/shopapp/entity/Foods.java
@@ -1,6 +1,7 @@
 package com.spring.shopapp.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
