1717b0eb125b8054963750c50d83fa196adf75f0
Teodor <te0gop.u@gmail.com>
2022-03-20 13:21:47 +0200

    logging shop service

--- shop-app/src/main/java/com/spring/shopapp/service/ShopService.java
+++ shop-app/src/main/java/com/spring/shopapp/service/ShopService.java
@@ -6,13 +6,19 @@ import com.spring.shopapp.entity.Shop;
 import com.spring.shopapp.repository.DrinksRepository;
 import com.spring.shopapp.repository.FoodsRepository;
 import com.spring.shopapp.repository.ShopRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ShopService {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(DrinkService.class);
 
     @Autowired
     ShopRepository shopRepository;
@@ -24,17 +30,21 @@ public class ShopService {
     private FoodsRepository foodsRepository;
 
     public ResponseEntity<Shop> createShop(Shop shop) {
+        LOGGER.warn("Creating new shop: " + shop.getShopName());
         shopRepository.save(shop);
+
+        shop.setFoods(foodsRepository.findAll());
+        shop.setDrinks(drinksRepository.findAll());
 
         for(Foods food : shop.getFoods()) {
             food.setShop(shop);
             foodsRepository.save(food);
         }
-
         for(Drinks drink : shop.getDrinks()) {
             drink.setShop(shop);
             drinksRepository.save(drink);
         }
+        LOGGER.info("Shop created.");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
