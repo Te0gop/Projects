2a237fd52fafd6ee7a760bee54a8832d40df15aa
Teodor <te0gop.u@gmail.com>
2022-03-21 21:54:26 +0200

    db

---
+++ students-restapi/students_rest_db.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA students_rest;
+USE students_rest;
+
+INSERT INTO students VALUES(1, 31, 'mike.h@gmail.com', 'Mike', 'Hensen');
+INSERT INTO students VALUES(2, 22, 'jake.h@gmail.com', 'Jake', 'Hex');
+INSERT INTO students VALUES(3, 60, 'jeff.b@gmail.com', 'Jeff', 'Bezos');
+
+INSERT INTO teachers VALUES(1, 60, 'Professor', 'Oscar', 'Reeds');
+INSERT INTO teachers VALUES(2, 49, 'PhD', 'Derek', 'Minh');
+
+INSERT INTO lectures VALUES(1, 2, 'Math', 1, 1, 2);
+INSERT INTO lectures VALUES(1, 2, 'Philosophy', 1, 2, 2);
+
