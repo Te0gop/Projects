0a9761767cf7c66b45c6cada8a329cde5f1afee7
Teodor <te0gop.u@gmail.com>
2022-04-16 12:37:59 +0300

    adding simples roles for accessing data

--- company/src/main/java/com/project/company/controller/DirectorateController.java
+++ company/src/main/java/com/project/company/controller/DirectorateController.java
@@ -7,10 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
 @RequestMapping("/directorates")
+@RolesAllowed("DIRECTOR")
 public class DirectorateController {
 
     @Autowired
--- company/src/main/java/com/project/company/controller/EmployeeController.java
+++ company/src/main/java/com/project/company/controller/EmployeeController.java
@@ -6,10 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
+@RolesAllowed({"CHIEF","DIRECTOR"})
 public class EmployeeController {
 
     @Autowired
