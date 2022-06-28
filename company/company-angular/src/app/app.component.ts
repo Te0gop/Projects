7d2da0562dc7007f9dcedfbe9f73717826aea16d
unknown <Te0gop.u@gmail.com>
2022-06-28 15:14:03 +0300

    modifying front-end

--- company/company-angular/src/app/app.component.ts
+++ company/company-angular/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee/employee';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { EmployeeService } from './employee/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'company-angular';
+export class AppComponent implements OnInit{
+  public employees: Employee[] | undefined;
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => alert(error.message));
+  }
+
 }
--- company/company-angular/src/app/app.module.ts
+++ company/company-angular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { EmployeeComponent } from './employee/employee.component';
     EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
