7eb14bee52edf95b3ee2d4ad3b61749f4d2dd125
unknown <Te0gop.u@gmail.com>
2022-06-29 10:43:11 +0300

    adding department service

--- company/company-angular/src/app/app.module.ts
+++ company/company-angular/src/app/app.module.ts
@@ -7,19 +7,26 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeService } from './employee/employee.service';
 import { DirectorateComponent } from './directorate/directorate.component';
+import { DirectorateService } from './directorate/directorate.service';
+import { DepartmentComponent } from './department/department.component';
+import { DepartmentService } from './department/department.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
-    DirectorateComponent
+    DirectorateComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule
   ],
-  providers: [EmployeeService],
+  providers: [EmployeeService, 
+    DirectorateService,
+    DepartmentService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
